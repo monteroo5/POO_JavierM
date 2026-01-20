e950678fca9da7dd455ac6847de4799054f89c05
monteroo5 <jmonteroetg@gmail.com>
2026-01-20 10:53:43 +0100

    form-login

---
+++ resources/views/components/form-login.php
@@ -0,0 +1,6 @@
+<form action="login.php" method="POST">
+    <label>DNI: <input type="text" name="dni" required></label><br>
+    <label>Contraseña: <input type="password" name="password" required></label><br>
+    <label><input type="checkbox" name="remember"> Permanecer conectado</label><br>
+    <button type="submit" name="btn_login">Entrar</button>
+</form>
