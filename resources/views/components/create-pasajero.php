4840336ab2c261fe6fbddfede7bccf4cb413f7f4
monteroo5 <jmonteroetg@gmail.com>
2026-01-20 10:53:13 +0100

    create-pasajero

---
+++ resources/views/components/create-pasajero.php
@@ -0,0 +1,5 @@
+<form action="index.php" method="POST">
+    <h4>Añadir Nuevo Pasajero</h4>
+    <label>Nº Asiento: <input type="number" name="numAsiento" required></label><br>
+    <button type="submit" name="btn_create_pasajero">Registrar Pasajero</button>
+</form>
